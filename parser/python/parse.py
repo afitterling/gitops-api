c83f8bf4366e072d58d405603e49d713448b6e22
Alexander Fitterling <info@sp33c.de>
2021-04-11 11:51:29 +0200

    parse

--- parser/python/parse.py
+++ parser/python/parse.py
@@ -5,10 +5,17 @@ from prettyprinter import cpprint
 with open("specs/0.1/api.yaml", 'r') as stream:
     model = yaml.safe_load(stream)
 
-#cpprint(model)
+cpprint(model)
 
 version = model.get('version')
-models = list(model.get('models').keys())
+
+kresources = list(model.get('resources').keys())
+kmodels = list(model.get('models').keys())
+kapis = list(model.get('apis').keys())
+kendpoints = list(model.get('endpoints').keys())
 
 cpprint(version)
-cpprint('models: ' + str(models))
+cpprint('apis: ' + str(kapis))
+cpprint('endpoints: ' + str(kendpoints))
+cpprint('models: ' + str(kmodels))
+cpprint('resources: ' + str(kresources))
