1aa90ccd7ecea6dbec5c98e08d3873200be65239
Alexander Fitterling <info@sp33c.de>
2021-04-11 11:22:55 +0200

    changed grammer to pluralized nodes with sub nodes

--- parser/python/parse.py
+++ parser/python/parse.py
@@ -5,5 +5,7 @@ from prettyprinter import cpprint
 with open("specs/0.1/api.yaml", 'r') as stream:
     model = yaml.safe_load(stream)
 
-cpprint(model)
+#cpprint(model)
 
+cpprint(model.get('version'))
+cpprint(model.get('models'))
