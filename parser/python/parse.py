3c8762c3a369eb8529ed35b9848a60ad6cfb4b5f
Alexander Fitterling <info@sp33c.de>
2021-04-11 11:02:12 +0200

    enpints

--- parser/python/parse.py
+++ parser/python/parse.py
@@ -2,7 +2,7 @@ import yaml
 import io
 from prettyprinter import cpprint
 
-with open("specs/api_v0.1.yaml", 'r') as stream:
+with open("specs/0.1/api.yaml", 'r') as stream:
     data_loaded = yaml.safe_load(stream)
 
 cpprint(data_loaded)
