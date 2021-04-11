71470734938504d3a7a9fa4bc9b7f7ce955cdef6
Alexander Fitterling <info@sp33c.de>
2021-04-11 11:19:34 +0200

    renamed model

--- parser/python/parse.py
+++ parser/python/parse.py
@@ -3,6 +3,7 @@ import io
 from prettyprinter import cpprint
 
 with open("specs/0.1/api.yaml", 'r') as stream:
-    data_loaded = yaml.safe_load(stream)
+    model = yaml.safe_load(stream)
 
-cpprint(data_loaded)
+cpprint(model)
+
